ddcd9565fa37dfbf05470e8711d883d7dda738a4
Ujjwal <39565250+ujjwalll@users.noreply.github.com>
2018-08-31 21:00:54 +0530

    hackerrank
    Solution to hacker rank problem statement.

---
+++ ViralAdvertisement.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the viralAdvertising function below.
+def viralAdvertising(n):
+    a=2
+    b=2
+    for i in range(1,n):
+        a*=3
+        a=math.floor(a/2)
+        b=b+a
+    return b
+    
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    result = viralAdvertising(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
